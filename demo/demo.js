d708de6e54aaa99d3d8678c89f01f72d7750663f
Onurcan TAŞBAŞ <onurcan.tasbas.cse@gmail.com>
2024-10-27 22:28:36 +0300

    v3

--- demo/demo.js
+++ demo/demo.js
@@ -357,7 +357,7 @@
 
   const MENU_ITEMS_DEFAULT = {
     insertColumnLeft: {
-      text: '在左侧插入一列',
+      text: 'Sola sütun ekle',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.appendCol();
@@ -365,7 +365,7 @@
       },
     },
     insertColumnRight: {
-      text: '在右侧插入一列',
+      text: 'Sağa sütun ekle',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.appendCol(true);
@@ -373,7 +373,7 @@
       },
     },
     insertRowTop: {
-      text: '在上方插入一行',
+      text: 'Üste satır ekle',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.appendRow();
@@ -381,7 +381,7 @@
       },
     },
     insertRowBottom: {
-      text: '在下方插入一行',
+      text: 'Alta satır ekle',
       groupEnd: true,
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
@@ -390,7 +390,7 @@
       },
     },
     removeCol: {
-      text: '删除所在列',
+      text: 'Sütunu sil',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.removeCol();
@@ -398,7 +398,7 @@
       },
     },
     removeRow: {
-      text: '删除所在行',
+      text: 'Satırı sil',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.removeRow();
@@ -406,7 +406,7 @@
       },
     },
     removeTable: {
-      text: '删除表格',
+      text: 'Tabloyu sil',
       groupEnd: true,
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
@@ -415,7 +415,7 @@
       },
     },
     mergeCell: {
-      text: '合并单元格',
+      text: 'Hücreleri birleştir',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.mergeCells();
@@ -423,7 +423,7 @@
       },
     },
     splitCell: {
-      text: '拆分单元格',
+      text: 'Hücreyi böl',
       groupEnd: true,
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
@@ -432,7 +432,7 @@
       },
     },
     setBackgroundColor: {
-      text: '设置背景颜色',
+      text: 'Arka plan rengini ayarla',
       isColorChoose: true,
       handler(color) {
         const tableModule = this.quill.getModule(moduleName.table);
@@ -440,14 +440,14 @@
       },
     },
     clearBackgroundColor: {
-      text: '清除背景颜色',
+      text: 'Arka plan rengini temizle',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.setStyle({ backgroundColor: null }, this.selectedTds);
       },
     },
     setBorderColor: {
-      text: '设置边框颜色',
+      text: 'Kenarlık rengini ayarla',
       isColorChoose: true,
       handler(color) {
         const tableModule = this.quill.getModule(moduleName.table);
@@ -455,7 +455,7 @@
       },
     },
     clearBorderColor: {
-      text: '清除边框颜色',
+      text: 'Kenarlık rengini temizle',
       handler() {
         const tableModule = this.quill.getModule(moduleName.table);
         tableModule.setStyle({ borderColor: null }, this.selectedTds);
@@ -468,11 +468,11 @@
     options = {
       items: {
         functionName: {
-          text: '显示文字',
-          handle() {},    // 触发事件
-          iconSrc: string,    // icon url
-          groupEnd: boolean, // 是否显示分隔线
-          subTitle: '显示子标题',
+          text: 'Görüntülenen metin',
+          handle() {},    // tetikleme olayı
+          iconSrc: string,    // ikon url'si
+          groupEnd: boolean, // ayırıcı çizgi göster
+          subTitle: 'Alt başlık göster',
         }
       }
     }
